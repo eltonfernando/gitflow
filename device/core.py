0512cdf9adc60359ddd86dd92d5540e99cf64a39
elton <eltonfernando90@gmail.com>
2023-09-01 17:02:06 -0300

    docs(all): add

--- device/core.py
+++ device/core.py
@@ -7,6 +7,17 @@ class Device:
         self.cam = Client(f"http://{ip}", user, password, timeout=10)
 
     def start(self) -> None:
+        """
+        Starts the execution of the program.
+
+        This function enters an infinite loop and repeatedly performs the following steps:
+        1. Prints the string "loop".
+        2. Retrieves an event using the `get_event()` method.
+        3. Checks if the retrieved event is a motion event using the `is_motion()` method.
+        4. If the event is a motion event, calls the `get_frame()` method.
+
+        This function does not take any parameters and does not return any value.
+        """
         while True:
             print("loop")
             event = self.get_event()
@@ -14,6 +25,15 @@ class Device:
                 self.get_frame()
 
     def get_event(self) -> dict:
+        """
+        Retrieves an event from the alert stream.
+
+        Returns:
+            dict: The retrieved event from the alert stream.
+
+        Raises:
+            Exception: If an error occurs during the retrieval process.
+        """
         try:
             response = self.cam.Event.notification.alertStream(
                 method="get", type="stream"
@@ -25,6 +45,15 @@ class Device:
             print(error)
 
     def is_motion(self, data_event: dict) -> None:
+        """
+        Checks if the given data event represents a motion event.
+
+        Parameters:
+            - data_event (dict): The data event to be checked.
+
+        Returns:
+            - bool: True if the event represents motion, False otherwise.
+        """
         data = data_event.get("EventNotificationAlert")
         event_status = data.get("eventState")
         if event_status == "inactive":
@@ -33,6 +62,17 @@ class Device:
             return True
 
     def get_frame(self) -> None:
+        """
+        Get the current frame from the camera and save it as an image file.
+
+        This function retrieves the current frame from the camera's streaming channel and saves it as a JPEG image file named "screen.jpg". The frame is obtained by making a GET request to the camera's streaming channel, specifying the picture method with the type parameter set to "opaque_data". The response is then iterated over in chunks of size 1024 and each chunk is written to the file "screen.jpg" using a binary write mode.
+
+        Parameters:
+        - None
+
+        Returns:
+        - None
+        """
         response = self.cam.Streaming.channels[102].picture(
             method="get", type="opaque_data"
         )
