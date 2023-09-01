bd7fdb7bb14d4a15e835767cae9aaedfbd6bfaf6
elton <eltonfernando90@gmail.com>
2023-09-01 16:37:22 -0300

    feat(main): captura evento0 de camera

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("inico")
+import json
+from hikvisionapi import Client
+cam = Client("http://192.168.237.120", "admin", "ac22446688", timeout=10)
+
+__VERSION__ = "0.0.1"
+print(f"Version: {__VERSION__}")
+def get_frame(response):
+    response = cam.Streaming.channels[102].picture(method="get", type="opaque_data")
+    with open("screen.jpg", "wb") as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+def get_event():
+    response = cam.Event.notification.alertStream(method="get", type="stream")
+    if response:
+        print(json.dumps(response, indent=4))
+        return response
+    
+def is_motion(response):
+    event = response[0]["EventNotificationAlert"]["eventState"]
+    return event
+
+
+while True:
+    try:
+        event = get_event()  
+        if is_motion(event) == "active":
+                get_frame(event)
+    except Exception:
+        pass
